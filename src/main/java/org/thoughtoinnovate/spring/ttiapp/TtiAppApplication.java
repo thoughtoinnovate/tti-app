6509707bdb0374163ff028a56e254ac622175a58
hiteshunk <hitesh.thinktank@gmail.com>
2022-03-20 17:50:51 +0530

    Init

---
+++ src/main/java/org/thoughtoinnovate/spring/ttiapp/TtiAppApplication.java
@@ -0,0 +1,13 @@
+package org.thoughtoinnovate.spring.ttiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/thoughtoinnovate/spring/ttiapp/TtiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thoughtoinnovate.spring.ttiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
